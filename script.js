e40d60e7f82dfe2005e1421ef54a743770efc8a2
FrankmDev <frankmmarindev@gmail.com>
2023-08-14 18:15:27 +0200

    Tip Calculator 1.0

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const input = document.getElementById("bill");
+const sliders = document.querySelectorAll("input[type='range']");
+
+sliders.forEach(function (slider) {
+  slider.addEventListener("input", calcTip);
+});
+
+function calcTip() {
+  let bill = parseFloat(input.value);
+  let tip = parseFloat(document.getElementById("tip-value").value);
+  let people = parseFloat(document.getElementById("people").value);
+
+  let totalTip = parseFloat((bill * (tip / 100)).toFixed(2));
+  let total = parseFloat((bill + totalTip).toFixed(2));
+
+  let tipPerson = (totalTip / people).toFixed(2);
+  let totalPerson = (total / people).toFixed(2);
+
+  document.querySelector(".tip-per-person").textContent = tipPerson;
+  document.querySelector(".total-per-person").textContent = totalPerson;
+
+  document.querySelector(".tip-span").textContent = tip;
+  document.querySelector(".people-span").textContent = people;
+}
+
+input.addEventListener("input", calcTip);
